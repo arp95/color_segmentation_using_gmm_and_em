3f20d91b9b924feae5ad943590e15a8156ecb06f
Arpit Aggarwal <arpitdec5@gmail.com>
2020-04-05 23:16:17 -0400

    Worked on yellow 1D gauss case

--- Code/yellow_buoy_1D_gauss.py
+++ Code/yellow_buoy_1D_gauss.py
@@ -29,6 +29,8 @@ from utils import *
 import sys
 
 # set data path
+k = 3
+d = 1
 args = sys.argv
 video_path = ""
 file_path = ""
@@ -44,7 +46,7 @@ training_data2 = training_data2[:25000, :]
 training_data = np.vstack((training_data1, training_data2))
 
 # get the weights, mean and variances of gaussian
-(weights_gaussian, mean_gaussian, covariance_matrix_gaussian) = run_expectation_maximization_algorithm(training_data.shape[0], 1, 2, 30, training_data)
+(weights_gaussian, mean_gaussian, covariance_matrix_gaussian) = run_expectation_maximization_algorithm(training_data.shape[0], d, k, 500, training_data)
 
 print(weights_gaussian)
 print(mean_gaussian)
@@ -63,32 +65,42 @@ while (cap.isOpened()):
     image1 = frame[:, :, 1].ravel()
     image2 = frame[:, :, 2].ravel()
     image = np.concatenate((image1, image2), axis=0)
-    image = np.reshape(image, (image.shape[0], 1))
-    prob = np.zeros((image.shape[0], 2))
-    likelihood = np.zeros((image.shape[0], 2))
-    for index in range(0, 2):
+    image = np.reshape(image, (image.shape[0], d))
+    prob = np.zeros((image.shape[0], k))
+    likelihood = np.zeros((image.shape[0], k))
+    for index in range(0, k):
         prob[: ,index:index+1] = gaussian_estimation_array(image, mean_gaussian[index], covariance_matrix_gaussian[index], 1) * weights_gaussian[index]
         likelihood = prob.sum(1)
     
     # pre-process image and create a binary image
-    prob_green = likelihood[: frame.shape[0] * frame.shape[1]]
-    prob_red = likelihood[frame.shape[0] * frame.shape[1]:]
-    prob = np.add(prob_red, prob_green)
-    prob[prob > np.max(prob) / 2.0] = 255
+    prob_green = likelihood[: int(frame.shape[0] * frame.shape[1])]
+    prob_red = likelihood[int(frame.shape[0] * frame.shape[1]):]
+    prob = np.add(prob_green, prob_red)
+    prob[prob_red > np.max(prob_red) / 2] = 255
     output_image = np.zeros_like(frame)
-    output_image[:, :, 1] = np.reshape(prob, (frame.shape[0], frame.shape[1]))
     output_image[:, :, 2] = np.reshape(prob, (frame.shape[0], frame.shape[1]))
+    output_image[:, :, 1] = np.reshape(prob, (frame.shape[0], frame.shape[1]))
     gray_output_image = cv2.cvtColor(output_image, cv2.COLOR_BGR2GRAY)
-    output_image = cv2.GaussianBlur(gray_output_image, (3, 3), 0)
-    _, edged = cv2.threshold(output_image, 20, 255, 0)
+    output_image = cv2.GaussianBlur(gray_output_image, (5, 5), 4)
+    _, edged = cv2.threshold(output_image, 225, 255, 0)
+    kernel2 = np.ones((3,3), np.uint8)
+    edged = cv2.dilate(edged, kernel2, iterations = 6)
     
     # find contours and segment the orange buoy
     (cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2:]
-    cnts, _ = sort_contours(cnts, method="right-to-left")
-    hull = cv2.convexHull(cnts[0])
-    (x, y), radius = cv2.minEnclosingCircle(hull)
-    if radius > 5:
-        cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-        out.write(frame)
-    else:
-        out.write(frame)
+    x_min = 1000
+    optimal_x = -1
+    optimal_y = -1
+    optimal_radius = -1
+    for contour in cnts:
+        if(cv2.contourArea(contour) > 250):
+            (x,y), radius = cv2.minEnclosingCircle(contour)
+            if(radius > 12 and radius < 50):
+                if(int(x) < x_min):
+                    x_min = x
+                    optimal_x = x
+                    optimal_y = y
+                    optimal_radius = radius
+    if(optimal_radius != -1):
+        cv2.circle(frame, (int(optimal_x), int(optimal_y)), int(optimal_radius), (0, 255, 255), 5)
+    out.write(frame)
